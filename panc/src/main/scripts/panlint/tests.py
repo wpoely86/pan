144663518546a726526066f1cd2d1f334b4512a7
Ward Poelmans <wpoely86@gmail.com>
2019-11-07 15:25:03 +0100

    fix tests

--- panc/src/main/scripts/panlint/tests.py
+++ panc/src/main/scripts/panlint/tests.py
@@ -19,6 +19,7 @@ import glob
 import unittest
 from sys import argv
 from os.path import basename, dirname, join
+import six
 
 import panlint
 
@@ -95,7 +96,7 @@ class TestPanlint(unittest.TestCase):
             'fn2': 'variable a = afunction() + 31;',
             'for': 'for (idx = 31; idx >= 0; idx = idx - 1) {',
             'square_brackets': 'variable x = b[c-1];',
-            'negative':  'variable x = -1;',
+            'negative': 'variable x = -1;',
             # lines that start or end with an operator (i.e. are part of a multi-line expression) should be allowed
             'line_cont': '+ 42;',
             'line_to_be_cont': 'variable x = 42 +',
@@ -128,7 +129,6 @@ class TestPanlint(unittest.TestCase):
                          (False, dgn_square_brackets, 'Unwanted space in simple expression in square brackets'))
         self.assertEqual(lc.whitespace_around_operators(bad_negative, []),
                          (False, dgn_negative, 'Unwanted space after minus sign (not operator)'))
-
 
     def test_whitespace_after_semicolons(self):
         bad_1 = 'foreach(k; v;  things) {'
@@ -198,7 +198,7 @@ class TestPanlint(unittest.TestCase):
         self.assertEqual(panlint.lint_line(good_first, 1, [], True), ([], set(), 0, False))
 
         diagnoses, messages, problem_count, first_line = panlint.lint_line(bad_first, 1, [], True)
-        self.assertEqual(diagnoses, ['^'*len(bad_first)])
+        self.assertEqual(diagnoses, ['^' * len(bad_first)])
         self.assertNotEqual(messages, set())
         self.assertEqual(problem_count, 1)
         self.assertEqual(first_line, False)
@@ -226,7 +226,7 @@ class TestPanlint(unittest.TestCase):
 
         # Test that all three check types co-exist
         diagnoses, messages, problem_count, first_line = panlint.lint_line('  "/software/components/foo/bar" = 42+7;', 7, [], False)
-        self.assertItemsEqual(diagnoses, ['^^', '                        ^^^', '                                    ^^^'])
+        six.assertCountEqual(self, diagnoses, ['^^', '                        ^^^', '                                    ^^^'])
         self.assertNotEqual(messages, set())
         self.assertEqual(problem_count, 3)
         self.assertEqual(first_line, False)
@@ -242,7 +242,7 @@ class TestPanlint(unittest.TestCase):
         'simon' : string = 'says';
         '''
 
-        self.assertItemsEqual(panlint.find_annotation_blocks(test_text), [2, 7])
+        six.assertCountEqual(self, panlint.find_annotation_blocks(test_text), [2, 7])
         self.assertEqual(panlint.find_annotation_blocks('template garbage;\n\n# Nothing to see here.\n\n'), [])
 
     def test_find_heredoc_blocks(self):
@@ -257,7 +257,7 @@ class TestPanlint(unittest.TestCase):
         EOFF
         '''
 
-        self.assertItemsEqual(panlint.find_heredoc_blocks(test_text), [4, 5, 8, 9])
+        six.assertCountEqual(self, panlint.find_heredoc_blocks(test_text), [4, 5, 8, 9])
         self.assertEqual(panlint.find_heredoc_blocks('template garbage;\n\n# Nothing to see here.\n\n'), [])
 
     def test_component_use(self):
@@ -298,6 +298,7 @@ class TestPanlint(unittest.TestCase):
             panlint.lint_line('    #' + line_prefix, 200, [], False),
             ([], set(), 0, False)
         )
+
 
 if __name__ == '__main__':
     unittest.main()
